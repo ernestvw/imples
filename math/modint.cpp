70a97058ffe8138708e61d56c71157429345020a
ernestvw <evw0301@gmail.com>
2020-04-27 10:37:08 +0200

    initial commit

---
+++ math/modint.cpp
@@ -0,0 +1,43 @@
+// -- BEGIN MODINT TEMPLATE --
+
+template<const int32_t MOD>
+struct ModInt {
+	long long x;
+	ModInt() : x(0) {}
+	ModInt(long long u) : x(u) {}
+	ModInt& operator = (const ModInt &m) { x = m.x; return *this; }
+	friend bool operator == (const ModInt& a, const ModInt& b) { return a.x == b.x; }
+	friend bool operator != (const ModInt& a, const ModInt& b) { return a.x != b.x; }
+	friend bool operator < (const ModInt& a, const ModInt& b) { return a.x < b.x; }
+	friend bool operator > (const ModInt& a, const ModInt& b) { return a.x > b.x; }
+	friend bool operator <= (const ModInt& a, const ModInt& b) { return a.x <= b.x; }
+	friend bool operator >= (const ModInt &a, const ModInt& b) { return a.x >= b.x; }
+
+	ModInt& operator += (const ModInt& m) { x += m.x; if(x >= MOD) x -= MOD; return *this; }
+	ModInt& operator -= (const ModInt& m) { x -= m.x; if(x < 0LL) x += MOD; return *this; }
+	ModInt& operator *= (const ModInt& m) { x = (1LL * x * m.x) % MOD; return *this; }
+	
+	friend ModInt operator - (const ModInt& a) { ModInt res(a); if(res.x) res.x = MOD - res.x; return res; }
+	friend ModInt operator + (const ModInt& a, const ModInt& b) { return ModInt(a) += ModInt(b); }
+	friend ModInt operator - (const ModInt& a, const ModInt& b) { return ModInt(a) -= ModInt(b); }
+	friend ModInt operator * (const ModInt& a, const ModInt& b) { return ModInt(a) *= ModInt(b); }
+	
+	static long long fp(long long u, long long k) {
+		long long res = 1LL;
+		while(k > 0LL) {
+			if(k & 1LL) res = (res * u) % MOD;
+			u = (u * u) % MOD;
+			k /= 2LL;
+		}
+		return res;
+	}
+	
+	ModInt fastpow(long long k) { return ModInt(fp(x, k)); }
+	ModInt inv() { return ModInt(fp(x, MOD-2)); }
+	ModInt& operator /= (const ModInt& m) { return *this *= ModInt(m).inv(); }
+	friend ModInt operator / (const ModInt& a, const ModInt& b) { return ModInt(a) /= ModInt(b); }
+
+	friend ostream& operator << (ostream& out, const ModInt& a) { return out << a.x; }
+};
+
+// -- END MODINT TEMPLATE --
