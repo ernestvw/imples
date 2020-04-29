b3cdb40171e2c6a068494c54117915ec9ed129ee
ernestvw <evw0301@gmail.com>
2020-04-29 16:16:15 +0200

    modified template

---
+++ template.cpp
@@ -0,0 +1,105 @@
+/*
+	* ernestvw
+	* C++
+	* April 2020
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+#define x first
+#define y second
+#define pb push_back
+#define pii pair<int, int>
+#define veci vector<int>
+#define vecs vector<string>
+#define amax(x, v) x = max(x, (v))
+#define amin(x, v) x = min(x, (v))
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) (int)(v.size())
+#define FOR(i, n) for(int i = 0; i < (n); ++i)
+#define nl '\n'
+
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+template <class T>
+using PQ = priority_queue<T, vector<T>, greater<T>>;
+
+const int oo = 1e9; // INT_MAX
+const long long OO = 1e18; // LLONG_MAX
+const double eps = 1e-9;
+
+#define TEST_CASES 0
+
+void solve(int test_case);
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int T = 1;
+	if(TEST_CASES) cin >> T;
+	for(int t = 1; t <= T; t++)
+		solve(t);
+
+	return 0;
+}
+
+// ---------------------------------------------------
+
+void solve(int test_case) {
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
